00d0ce273d70c7872828c40a6082c3c78b156477
dhad-odoo <dhad@odoo.com>
2024-02-09 10:56:08 +0530

    initial structure of module created along with emoployee model

---
+++ management/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "management",
+    'depends': [
+        'base'
+    ],
+    'data':[
+        'security/ir.model.access.csv',
+    ],
+    'instable': True,
+    'autoinstall':False,
+    'application': True,
+}
